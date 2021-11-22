f02a71ee4fee747283c872aba640f7dc58fbcad1
Abdullah-Aljumah <Abdullah.Ali.elq.js@tuwaiq.edu.sa>
2021-11-22 11:55:30 +0300

    add bio in user schema

--- db/models/userSchema.js
+++ db/models/userSchema.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, require: true },
+  bio: { type: String },
   favorite: [{ type: mongoose.Schema.Types.ObjectId, ref: "Character" }],
 });
 
